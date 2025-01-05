fba8126b83857b88ebae19b740168029f32d5f3a
pedro-menezes <godofwar2906@gmail.com>
2025-01-04 21:33:56 -0300

    tentando

--- remix.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    serverBuildTarget: 'vite', // Usa Vite como o bundler para o servidor
-    appDirectory: 'src', // Diretório onde seu código está localizado, ajustando conforme necessário
-    assetsBuildDirectory: 'public/build', // Pasta de saída dos ativos compilados
-    publicPath: '/build/', // Caminho público para os arquivos construídos
-    // Outras opções de configuração do Remix podem ser adicionadas aqui conforme necessário
-  };
-  
